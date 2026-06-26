0b7baf695fba53e007f4357377ea2733213c8dea
koteswararaosiddu <koteswararaosiddu@gmail.com>
2026-06-27 01:09:20 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -53,7 +53,7 @@ export default function Sidebar() {
 
         {/* Mobile Image */}
         <div className="mt-10 flex justify-center lg:hidden">
-          <div className="h-28 w-28 overflow-hidden rounded-br-full border border-ink-600">
+          <div className="h-30 w-30 overflow-hidden rounded-full border border-ink-600">
             <Image
               src="/profile.jpg"
               alt={`Portrait of ${profile.name}`}
